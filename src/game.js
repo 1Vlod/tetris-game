9fd0b0399df04eca588227d79a1c33d54bf7e87f
Vlad <example@gmail.comg>
2020-08-17 20:14:27 +0300

    movePiece is made

--- src/game.js
+++ src/game.js
@@ -4,8 +4,6 @@ export default class Game {
   level = 0;
   playfield = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,15 +20,75 @@ export default class Game {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 0, 0, 0, 0]
   ];
   activePiece = {
     x: 0,
     y: 0,
     blocks: [
-      [0, 0, 0],
+      [0, 1, 0],
       [1, 1, 1],
       [0, 0, 0]
     ]
   };
+
+  movePieceLeft() {
+    this.activePiece.x -=1
+
+    if(this.hasCollision()) {
+      this.activePiece.x += 1
+    }
+  }
+
+  movePieceRight() {
+    this.activePiece.x +=1
+    
+    if(this.hasCollision()) {
+      this.activePiece.x -= 1
+    }
+  }
+
+  movePieceDown() {
+    this.activePiece.y +=1
+
+    if(this.hasCollision()) {
+      this.activePiece.y -= 1
+      this.lockPiece()
+    }
+  }
+
+  hasCollision() {
+    const {y: pieceY, x: pieceX, blocks} = this.activePiece
+    const playfield = this.playfield
+
+    for(let y = 0; y < blocks.length; y++) {
+      for(let x = 0; x < blocks[y].length; x++) {
+        if(blocks[y][x] && 
+         ((playfield[pieceY + y] === undefined || playfield[pieceY + y][pieceX + x] === undefined)||
+         playfield[pieceY + y][pieceX + x] === 1)
+         ) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  lockPiece() {
+    const {y: pieceY, x: pieceX, blocks} = this.activePiece
+
+    for(let y = 0; y < blocks.length; y++) {
+      for(let x = 0; x < blocks[y].length; x++) {
+        if(blocks[y][x]) {
+          this.playfield[pieceY + y][pieceX + x] = blocks[y][x]
+        }
+      }
+    }
+  }
+
+  // clearPlayfield() {
+  //   this.playfield = this.playfield.map(elem => elem.map(elem => elem = 0))
+  // }
 }
